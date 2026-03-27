651d2b6be0e690a9a9f728ce0d3b5b82012d3ee5
Mira <mira.theidel@gmx.de>
2026-03-27 23:52:33 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/fluid-path-3d-graph/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
